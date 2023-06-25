713175eabdbd9c3446e45c40704e8e535a330663
karpekin <karpekin@mail.ru>
2023-06-25 12:20:42 +0300

    task 18

--- homework3.py
+++ homework3.py
@@ -7,13 +7,35 @@
 3
 -> 1
 '''
+# from random import randint
+# N = abs(int(input('Введите N: ')))
+# A = [randint(1, N) for i in range(N)]
+# print(A)
+# X = abs(int(input('Введите X: ')))
+# summ = 0
+# for j in range(len(A)):
+#     if A[j] == X:
+#         summ += 1
+# print(summ)
+'''Задача 18: Требуется найти в массиве A[1..N] самый близкий по
+величине элемент к заданному числу X. Пользователь в первой строке
+вводит натуральное число N – количество элементов в массиве. В
+последующих строках записаны N целых чисел Ai. Последняя строка
+содержит число X
+5
+1 2 3 4 5
+6
+-> 5
+'''
 from random import randint
 N = abs(int(input('Введите N: ')))
 A = [randint(1, N) for i in range(N)]
 print(A)
 X = abs(int(input('Введите X: ')))
-summ = 0
-for j in range(len(A)):
-    if A[j] == X:
-        summ += 1
-print(summ)
+mini = max(A)
+j = 0
+for i in A:
+    if abs(X-i) < mini:
+        mini = abs(X-i)
+        j = i
+print(j)
