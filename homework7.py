38733bc5f0194cdcb3bd50d8504703b94ed17988
karpekin <karpekin@mail.ru>
2023-07-13 14:37:26 +0300

    task 36

--- homework7.py
+++ homework7.py
@@ -11,10 +11,37 @@
 Ввод: пара-ра-рам рам-пам-папам па-ра-па-да
 Вывод: Парам пам-пам'''
 
-data = 'пара-ра-рам рам-пам-папам па-ра-па-да'
+# data = 'пара-ра-рам рам-пам-папам па-ра-па-да'
+#
+# counts_a = [frase.count('а') for frase in data.split()]
+# all_equal = all(x == counts_a[0] for x in counts_a)
+# print('Парам пам-пам' if all_equal else 'Пам парам')
 
-counts_a = [frase.count('а') for frase in data.split()]
-all_equal = all(x == counts_a[0] for x in counts_a)
-print('Парам пам-пам' if all_equal else 'Пам парам')
+'''Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), 
+которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца. 
+Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, 
+которые должны быть распечатаны. Нумерация строк и столбцов идет с единицы 
+(подумайте, почему не с нуля). Примечание: бинарной операцией называется любая операция, 
+у которой ровно два аргумента, как, например, у операции умножения.
+
+*Пример:*
+
+**Ввод:** `print_operation_table(lambda x, y: x * y) ` 
+**Вывод:**
+1 2 3 4 5 6
+
+2 4 6 8 10 12
+3 6 9 12 15 18
+4 8 12 16 20 24
+5 10 15 20 25 30
+6 12 18 24 30 36'''
+
+def print_operation_table(operation, rows = 6, columns = 6):
+    for row in range(1, rows+1):
+        for col in range(1, columns+1):
+            print(operation(row, col), end='\t')
+        print()
+print_operation_table(lambda x, y: x * y)
+
 
 
